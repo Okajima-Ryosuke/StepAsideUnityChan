fc941d3bb68f098c2c3b167610947608557cca48
岡島諒介 <>
2019-11-02 19:47:57 +0900

    課題提出

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	public GameObject CoinClone;
+	public GameObject ConeClone;
+	public GameObject CarClone;
+
+	// ユニティ様の後方15メートルのコインと障害物をデストロイする内容をこのファイルに書く
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		CoinClone = GameObject.Find("CoinPrefab(Clone)");
+		ConeClone = GameObject.Find ("TrafficConePrefab(Clone)");
+		CarClone = GameObject.Find ("CarPrefab(Clone)");
+
+		if (CoinClone && this.transform.position.z > CoinClone.transform.position.z) {
+			Destroy (CoinClone);
+		}
+		if (ConeClone && this.transform.position.z > ConeClone.transform.position.z) {
+			Destroy (ConeClone);
+		}
+		if (CarClone && this.transform.position.z > CarClone.transform.position.z) {
+			Destroy (CarClone);
+		}
+	}
+}
---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+
+	public GameObject carPrefab;
+	public GameObject coinPrefab;
+	public GameObject conePrefab;
+	private int startPos = -160;
+	private int goalPos = 120;
+	private float posRange = 3.4f;
+	private int zpos = -115;
+	public GameObject UnitySama;
+
+	// Use this for initialization
+	void Start () {
+
+		UnitySama = GameObject.Find ("unitychan");
+
+		for (int i = startPos; i < startPos + 45 ; i += 15) {
+			int num = Random.Range (1, 11);
+			if (num <= 2) {
+				for (float j = -1; j <= 1; j += 0.4f) {
+					GameObject cone = Instantiate (conePrefab) as GameObject;
+					cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, i);
+				}
+			} else {
+				for (int j = -1; j <= 1; j++) {
+					int item = Random.Range (1, 11);
+					int offsetZ = Random.Range (-5, 6);
+					if (1 <= item && item <= 6) {
+						GameObject coin = Instantiate (coinPrefab) as GameObject;
+						coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, i + offsetZ);
+					} else if (7 <= item && item <= 9) {
+						GameObject car = Instantiate (carPrefab) as GameObject;
+						car.transform.position = new Vector3 (posRange * j, car.transform.position.y, i + offsetZ);
+					}
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	// ユニティ様の45メートル先から　15メートル毎に　ゴール地点まで　アイテム生成
+	void Update () {
+		int UnitySamaZ = (int)UnitySama.transform.position.z;
+		if (UnitySamaZ + 45 == zpos && UnitySamaZ + 45 < goalPos) {
+			int num = Random.Range (1, 11);
+			if (num <= 2) {
+				for (float j = -1; j <= 1; j += 0.4f) {
+					GameObject cone = Instantiate (conePrefab) as GameObject;
+					cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, zpos);
+				}
+			} else {
+				for (int j = -1; j <= 1; j++) {
+					int item = Random.Range (1, 11);
+					int offsetZ = Random.Range (-5, 6);
+					if (1 <= item && item <= 6) {
+						GameObject coin = Instantiate (coinPrefab) as GameObject;
+						coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, zpos + offsetZ);
+					} else if (7 <= item && item <= 9) {
+						GameObject car = Instantiate (carPrefab) as GameObject;
+						car.transform.position = new Vector3 (posRange * j, car.transform.position.y, zpos + offsetZ);
+					}
+				}
+			}
+			zpos += 15;
+		}
+	}
+}
